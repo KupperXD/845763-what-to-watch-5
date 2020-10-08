2783c9548a156784b32a68d5787bc005726b2215
Dmitriy Tarasenko <tarasenko@internet-design.ru>
2020-10-08 18:13:47 +0300

    Роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import Home from "../home/home";
+import AddReview from "../add-review/add-review";
+import Film from "../film/film";
+import MyList from "../my-list/my-list";
+import Player from "../player/player";
+import SignIn from "../sign-in/sign-in";
+import {BrowserRouter, Switch, Route} from "react-router-dom/";
 import PropTypes from "prop-types";
 
 const App = (props) => {
   const {name, style, date} = props;
 
   return (
-    <Home
-      name={name}
-      style={style}
-      date={date}
-    >
-
-    </Home>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home
+            name={name}
+            style={style}
+            date={date}
+          >
+          </Home>
+        </Route>
+        <Route path="/login/" exact>
+          <SignIn></SignIn>
+        </Route>
+        <Route path="/mylist" exact>
+          <MyList></MyList>
+        </Route>
+        <Route path="/films/:id" exact>
+          <Film></Film>
+        </Route>
+        <Route path="/films/:id/review" exact>
+          <AddReview></AddReview>
+        </Route>
+        <Route path="/player/:id" exact>
+          <Player></Player>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
